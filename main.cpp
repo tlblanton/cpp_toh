2fb38624ef1ec3a4bd9c37f59815971acca59429
tlblanton <tylerblanton635@gmail.com>
2016-02-22 17:28:32 -0700

    finished version with correct output. Uses classes with vectors and names intead of just vectors of the std::stack class

--- main.cpp
+++ main.cpp
@@ -1,61 +1,67 @@
 #include <iostream>
 #include <vector>
-#include <string>
 
 using namespace std;
 
 //I like to think that this namespace makes these global variables a little less bad, but in reality it may not. Boo globals.
-namespace tylerSpace
+class customStack
 {
-    vector<int> A;
-    vector<int> B;
-    vector<int> C;
+public:
+    vector<int> stack;
+    string name;
+    customStack(string nName) : name(nName)
+    {}
+};
+namespace tylerSpace{
+
+    int moves = 0;
+    customStack A("A");
+    customStack B("B");
+    customStack C("C");
 }
-
-
 using namespace tylerSpace;
 
-void display(vector<int> A, vector<int> B, vector<int> C);
+void display(customStack A, customStack B, customStack C);
 
-void hanoi(int rings, vector<int>& A, vector<int>& B, vector<int>& C);
-
-
-
+void hanoi(int rings, customStack& A, customStack& B, customStack& C);
 
 
 int main()
 {
     int rings;
-
-
     cout << "How many rings are on tower A?" << endl;
     cin >> rings;
     for(int i = rings; i > 0; --i)
     {
-        A.push_back(i);
+        A.stack.push_back(i);
     }
 
     display(A, B, C);
 
     hanoi(rings, A, B, C);
+    cout << "Number of Moves: " << moves << endl;
 
     return 0;
 }
 
 
-void hanoi(int rings, vector<int>& start, vector<int>& aux, vector<int>& end)
+void hanoi(int rings, customStack& start, customStack& aux, customStack& end)
 {
     if(rings == 1)
     {
-        end.push_back(start[start.size()-1]);
-        start.pop_back();
+        end.stack.push_back(start.stack[start.stack.size()-1]);
+        moves++;
+        start.stack.pop_back();
+        cout << start.name << " -> " << end.name << endl;
         display(A, B, C);
     }
     else
     {
         hanoi(rings-1, start, end, aux);
-        end.push_back(start[start.size()-1]);
-        start.pop_back();
+        end.stack.push_back(start.stack[start.stack.size()-1]);
+        moves++;
+        start.stack.pop_back();
+        cout << start.name << " -> " << end.name << endl;
         display(A, B, C);
 
         hanoi(rings-1, aux, start, end);
@@ -63,55 +69,57 @@ void hanoi(int rings, vector<int>& start, vector<int>& aux, vector<int>& end)
 }
 
 
-void display(vector<int> A, vector<int>B, vector<int>C)
+void display(customStack A, customStack B, customStack C)
 {
+    cout << "** Move Number: " << moves << endl;
+
     cout << "[";
-    for(int i = 0; i < A.size(); ++i)
+    for(int i = 0; i < A.stack.size(); ++i)
     {
-        if(i != A.size()-1)
+        if(i != A.stack.size()-1)
         {
-            cout << A[i] << ", ";
+            cout << A.stack[i] << ", ";
         }
         else
         {
-            cout << A[i] << "]" << endl;
+            cout << A.stack[i] << "]" << endl;
         }
     }
-    if(A.size() == 0)
+    if(A.stack.size() == 0)
     {
         cout << "]" << endl;
     }
 
     cout << "[";
-    for(int i = 0; i < B.size(); ++i)
+    for(int i = 0; i < B.stack.size(); ++i)
     {
-        if(i != B.size()-1)
+        if(i != B.stack.size()-1)
         {
-            cout << B[i] << ", ";
+            cout << B.stack[i] << ", ";
         }
         else
         {
-            cout << B[i] << "]" << endl;
+            cout << B.stack[i] << "]" << endl;
         }
     }
-    if(B.size() == 0)
+    if(B.stack.size() == 0)
     {
         cout << "]" << endl;
     }
 
     cout << "[";
-    for(int i = 0; i < C.size(); ++i)
+    for(int i = 0; i < C.stack.size(); ++i)
     {
-        if(i != C.size()-1)
+        if(i != C.stack.size()-1)
         {
-            cout << C[i] << ", ";
+            cout << C.stack[i] << ", ";
         }
         else
         {
-            cout << C[i] << "]" << endl;
+            cout << C.stack[i] << "]" << endl;
         }
     }
-    if(C.size() == 0)
+    if(C.stack.size() == 0)
     {
         cout << "]" << endl;
     }
